e48dc60f53334a8631620a9efcab30114033a143
rodrigo <r.aluizio@gmail.com>
2018-10-10 12:38:49 -0300

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='MSAJ',
+    version='1.0',
+    url='https://github.com/Godrigos/MSAJ',
+    license='GPL-3.0',
+    author='Rodrigo Aluizio',
+    author_email='',
+    description='Multi Sequence Alignment Joiner',
+    long_description=long_description,
+    entry_points={'gui_scripts': ["MSAJ = ui.py"]},
+    package_data={
+        '': ['icons/*.*'],
+    },
+    install_requires=['PyQt5', 'biopython'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0",
+        "Operating System :: OS Independent",
+    ],
+)
+
